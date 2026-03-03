15025ff3ccb5a0fecfbec72a396cb9c12e8ba8e9
shahzad11khan <zarakali401@gmail.com>
2026-03-03 15:02:47 +0500

    variables and datatypes , converstions

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,6 +1,66 @@
-console.log("hello world!");
-numberno = 1;
-numbertwo = 2;
-console.log(numberno + numbertwo);
-alert("web class");
-document.writeln("web class");
+// class one
+
+// console.log("hello world!");
+// numberno = 1;
+// numbertwo = 2;
+// console.log(numberno + numbertwo);
+// alert("web class");
+// document.writeln("web class");
+
+// class 2
+// variables
+
+// name = "web class";
+// console.log(name);
+
+// var , let , const
+
+// var name = "web class";
+// var name = "web class 1";
+// console.log(name);
+
+// let name = "web class";
+// name = "web class 1";
+// let name;
+// name = "web"
+// console.log(name);
+
+// primitive
+
+// const name = "web";
+// const age = 21;
+// const bolean = true;
+
+// console.log(name, age, bolean);
+
+// non primitive
+
+// const arr = [1, "web", true, 21];
+// console.log(arr);
+
+// const std = {
+// pro : value
+// color: "red",
+// name: "web",
+// age: 21,
+// bolean: true,
+// }
+
+// console.log(std.name);
+
+// conversio
+
+// console.log("5" + 5);
+// console.log(5 + 5);
+
+// console.log("1w" - 5);
+
+// explicit
+// const num = 5;
+// const str = "5";
+// console.log(num + Number(str));
+
+// const num = 5;
+// console.log(num.toString())
+
+
