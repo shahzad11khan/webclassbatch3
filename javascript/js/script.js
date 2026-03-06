6b3ed8f77e4ad9949928c66ac253049c8dd48204
shahzad11khan <zarakali401@gmail.com>
2026-03-06 14:29:18 +0500

    countinue and break statement and guess game with while loop

--- javascript/js/script.js
+++ javascript/js/script.js
@@ -1,228 +1,70 @@
-// class one
+// loops in js
+console.log("javascript");
+// console.log("javascript");
+// console.log("javascript");
+// console.log("javascript");
+// console.log("javascript");
 
-// console.log("hello world!");
-// numberno = 1;
-// numbertwo = 2;
-// console.log(numberno + numbertwo);
-// alert("web class");
-// document.writeln("web class");
+// for, while , do while
 
-// class 2
-// variables
-
-// name = "web class";
-// console.log("name");
-
-// name = "web class";
-// Name = "batchIII";
-// console.log(name, Name);
-
-// var , let , const
-
-// var name = "web class";
-// console.log(name);
-// var name = "web class 1";
-// console.log(name);
-
-// let name = "shahzad";
-// name = "web class 1";
-// console.log(name);
-
-// let name;
-// const Name = undefined;
-// console.log(name);
-
-// let name = "web class";
-// name = "web class 1";
-// let name;
-// name = "web"
-// console.log(name);
-
-
-// find type of variable
-
-// const name = "web";
-// const age = 27;
-// const bolean = true;
-// console.log(typeof age);
-
-
-// primitive
-
-// const name = "web";
-// const age = 21;
-// const bolean = true;
-
-// console.log(name, age, bolean);
-
-// non primitive
-
-// const arr = [1, "web", true, 21];
-// console.log(arr);
-
-// const std = {
-// pro : value
-// color: "red",
-// name: "web",
-// age: 21,
-// bolean: true,
+// increament/decreament
+// let a = 6;
+// ++  --> increment by one
+// --  --> decrement by one
+// console.log(a);
+// a = a - 2
+// a -= 2;
+// a += 2;
+// a *= 2;
+// console.log(++a);
+// console.log("prefix", --a);
+// console.log("postfix", a);
+// for(initialization; condition; increment/decrement){}
+// it run until condition is true with specified number of time
+// for (let i = 1; i <= 100; i++) {
+//     // body of for loop
+//     console.log("my web class");
 // }
 
-// console.log(std.name);
-
-// conversio
-
-// console.log("5" + 5);
-// console.log(5 + 5);
-
-// console.log("1w" - 5);
-
-// explicit
-// const num = 5;
-// const str = "5";
-// console.log(num + Number(str));
-
-// const num = 5;
-// console.log(num.toString())
-
-
-
-
-// object
-
-// css
-
-// selector{
-//  property : value
+// while loop
+// initialization
+// while(condition){
+// increment/decrement
 // }
 
-// const student = {
-//  property : value
-// key : value
-//     name: "ali",
-//     age: 21,
-//     ismale: true,
-// }
-// console.log(student);
-// console.log(student.name);
-
-// operators
-
-// arithmatic
-// + - * / %
-
-// console.log(5 + 5); // 10
-// console.log(5 - 5); // 0
-// console.log(5 * 5); // 25
-// console.log(5 / 5); // 1
-// console.log(5 % 5); // 0
-
-// prompt --> take input from user
-// const name = prompt("enter your name");
-// console.log(name);
-
-// comparison operators
-// < , > , == , === , ! , != , !== , <= , >=
-
-// const no = 100;
-// const no2 = 1000;
-// const no3 = 400;
-// const no4 = 10;
-// console.log(typeof "1 + 2 :" + no + no2);
-// console.log(typeof "1 + 2 :", typeof no + no2);
-
-// console.log("no == no2: ", !(no == no2)); // false // just check value
-// console.log("no === no2: ", no === no2); // false // check value and type
-
-
-// console.log(no < no2 && no3 > no4)
-
-// console.log(18 > 18);
-// console.log(18 == 18);
-// console.log(18 >= 18);
-
-// if , ifelse , elseif , switch
-
-// const name = "web";
-// let name = "class";
-// name = "web";
-// console.log(name);
-
-
-// const name = "web";
-// console.log(name);
-
-// block
-// {
-// var name = "class"
-// console.log(name)
+// let i = 1;
+// while (i >= 100) {
+//     // body of while loop
+//     console.log("my web class");
+//     i++;
 // }
 
-// let name;
-// const xyz;
+// console.log("end of while loop");
 
-// const name="haris";
-
-// {
-//     const name = "faisal"
-//     console.log(name)
-// }
-// const name = "xyz"
-// console.log(name)
+// do-while loop
+// let i = 1;
+// do {
+//     console.log("my web class");
+//     i++;
+// } while (i >= 100);
+// console.log("end of do-while loop");
 
 
-
-// const name = "haris";
-
-// console.log(name)
-// {
-//     const name = "faisal"
-//     console.log(name)
-// }
-// name = "xyz"
-// console.log(name)
-
-
-//  conditional statements
-
-// if , ifelse , elseif , switch
-
-// age 
-// if (1 > 4) {
-//     // body of if
-//     console.log("8 is greater than 4")
+// for (let index = 0; index <= 10; index++) {
+//     if (index === 5) {
+//         // continue;// skip
+//         break;// stop
+//     }
+//     console.log(index);
 // }
 
-// let age = 18;
-// if (age >= 18) {
-//     // body of if
-//     console.log("greater")
-// } else {
-//     // body of else
-//     console.log("small")
-// }
-
-// else if 
-// let age = 1;
-
-// if (age > 18) {
-//     console.log("age is greater")
-// } else if (age == 18) {
-//     console.log("age is equal")
-// } else {
-//     console.log("default");
-// }
-
-// switch
-let age = 18;
-switch (age) {
-    case 18:
-        console.log("age is 18");
-    // break;
-    case 19:
-        console.log("age is 19");
-        break;
-    default:
-        console.log("default");
-        break;
+// 
+let password = Number(prompt("Enter your password"));
+let db_password = 1357;
+// console.log(password, db_password)
+// // console.log(password == db_password)
+while (password != db_password) {
+    password = Number(prompt("Enter your password"));
+    console.log("passowrd is not correct!!")
 }
+console.log("password is correct!!")
